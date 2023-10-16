bdb6ba24537d41e476767d9eb5b2f5fdb05cd8b3
Patrick Chan <q_p18@hotmail.com>
2023-10-16 21:47:20 +0100

    Create read_db_creds() method.

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,8 @@
+import yaml
+
 class DatabaseConnector:
-    pass
+    
+    def read_db_creds() -> dict:
+        with open('db_creds.yaml', 'r') as file:
+            db_creds = yaml.safe_load(file)
+            return db_creds
