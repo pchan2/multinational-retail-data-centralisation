d0476d411358d37cc85c46cdf2a29a60c77e2ced
Patrick Chan <q_p18@hotmail.com>
2023-10-16 23:01:53 +0100

    Create the list_db_tables() method.

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,11 @@
-class DataExtractor:
+from database_utils import DatabaseConnector
+
+
+class DataExtractor():
     
-    def __init__(self):
+    def __init__(self) -> None:
         print('Initialise the new instance of DataExtractor.')
+    
+    def list_db_tables(self) -> None:
+        engine = DatabaseConnector().init_db_engine()
+        print(engine.table_names())
