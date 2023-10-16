c60ce47343abe9abd927630d6e93ce994f1d9fe8
Patrick Chan <q_p18@hotmail.com>
2023-10-16 22:22:44 +0100

    Create __init__() and init_db_engine() methods.

--- database_utils.py
+++ database_utils.py
@@ -1,8 +1,26 @@
 import yaml
 
+from sqlalchemy import create_engine, engine
+
+
 class DatabaseConnector:
     
-    def read_db_creds() -> dict:
+    def __init__(self):
+        print('Initialise the new instance of DatabaseConnector.')
+
+    def read_db_creds(self) -> dict:
         with open('db_creds.yaml', 'r') as file:
             db_creds = yaml.safe_load(file)
             return db_creds
+
+    def init_db_engine(self) -> engine.base.Engine:
+        db_creds = self.read_db_creds()
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        ENDPOINT = db_creds['RDS_HOST']
+        USER = db_creds['RDS_USER']
+        PASSWORD = db_creds['RDS_PASSWORD']
+        PORT = db_creds['RDS_PORT']
+        DATABASE = db_creds['RDS_DATABASE']
+        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
+        return engine
