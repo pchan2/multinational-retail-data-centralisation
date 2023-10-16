c794d57e7977a109bfc51ca9855e333b62479054
Patrick Chan <q_p18@hotmail.com>
2023-10-16 23:25:34 +0100

    Refactor __init__() and list_db_tables() methods. Create read_rds_table() method.

--- data_extraction.py
+++ data_extraction.py
@@ -1,11 +1,18 @@
+import pandas as pd
+
 from database_utils import DatabaseConnector
+from pandas.core.frame import DataFrame
 
 
 class DataExtractor():
     
     def __init__(self) -> None:
-        print('Initialise the new instance of DataExtractor.')
+        self.database_connector = DatabaseConnector()
+        self.engine = self.database_connector.init_db_engine()
     
-    def list_db_tables(self) -> None:
-        engine = DatabaseConnector().init_db_engine()
-        print(engine.table_names())
+    def list_db_tables(self) -> list:
+        return self.engine.table_names()
+    
+    def read_rds_table(self, table_name: str) -> DataFrame:
+        df = pd.read_sql_table(table_name, self.engine)
+        return df
