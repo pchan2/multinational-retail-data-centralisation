8e3eb4edd5c1e8d836bc06648cc9a0bcd8e19c66
Patrick Chan <q_p18@hotmail.com>
2023-10-19 03:28:58 +0100

    Tidy code.

--- database_utils.py
+++ database_utils.py
@@ -1,19 +1,20 @@
 import yaml
 
-from sqlalchemy import create_engine, engine
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
 
 
-class DatabaseConnector:
+class DatabaseConnector():
     
-    def __init__(self):
+    def __init__(self) -> None:
         print('Initialise the new instance of DatabaseConnector.')
 
     def read_db_creds(self) -> dict:
-        with open('db_creds.yaml', 'r') as file:
+        with open('../db_creds.yaml', 'r') as file:
             db_creds = yaml.safe_load(file)
             return db_creds
 
-    def init_db_engine(self) -> engine.base.Engine:
+    def init_db_engine(self) -> Engine:
         db_creds = self.read_db_creds()
         DATABASE_TYPE = 'postgresql'
         DBAPI = 'psycopg2'
