52b7045367b745ce27656bbb161cdec35ccc71c1
Patrick Chan <q_p18@hotmail.com>
2023-10-24 02:51:02 +0100

    Create list_number_of_stores().

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 
 from database_utils import DatabaseConnector
 from pandas.core.frame import DataFrame
@@ -10,6 +11,7 @@ class DataExtractor():
     def __init__(self) -> None:
         self.database_connector = DatabaseConnector()
         self.engine = self.database_connector.init_db_engine()
+        self.headers = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
     
     def list_db_tables(self) -> list:
         return self.engine.table_names()
@@ -22,3 +24,15 @@ class DataExtractor():
         dfs = read_pdf(pdf_link, pages='all')
         df = pd.concat(dfs)
         return df
+
+    def list_number_of_stores(self, number_of_stores_endpoint: str) -> None:
+        # https://stackoverflow.com/questions/5517281/place-api-key-in-headers-or-url
+        # Custom Header: curl -H "X-API-KEY: 6fa741de1bdd1d91830ba" https://api.mydomain.com/v1/users
+        # curl -H 'x-api-key: yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX' https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores
+        res = requests.get(number_of_stores_endpoint, headers=self.headers)
+        number_stores = res.json()['number_stores'] # {'statusCode': 200, 'number_stores': 451}
+        print('Number of stores:', number_stores,) 
+
+
+NUMBER_OF_STORES_ENDPOINT = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+DataExtractor().list_number_of_stores(NUMBER_OF_STORES_ENDPOINT) # 451
