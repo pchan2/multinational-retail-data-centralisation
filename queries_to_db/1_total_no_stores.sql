feef62657dc820100aa012f00e7983d070933449
Patrick Chan <q_p18@hotmail.com>
2023-11-05 06:22:53 +0000

    Query the number of stores per country.

---
+++ queries_to_db/1_total_no_stores.sql
@@ -0,0 +1,19 @@
+-- How many stores does the business have and in which countries?
+
+-- The Operations team would like to know which countries we currently operate
+-- in and which country now has the most stores. Perform a query on the
+-- database to get the information, it should return the following information:
+
++----------+-----------------+
+| country  | total_no_stores |
++----------+-----------------+
+| GB       |             265 |
+| DE       |             141 |
+| US       |              34 |
++----------+-----------------+
+
+SELECT DISTINCT(country_code) AS country,
+    COUNT(country_code) AS total_no_stores
+FROM dim_store_details
+GROUP BY dim_store_details.country_code
+ORDER BY total_no_stores DESC;
