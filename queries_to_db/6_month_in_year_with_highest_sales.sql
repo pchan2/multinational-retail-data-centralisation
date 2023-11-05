c25169891c903baabd9ed1184029c0bd604042d5
Patrick Chan <q_p18@hotmail.com>
2023-11-05 15:47:01 +0000

    Query the month in each year with the highest sales.

---
+++ queries_to_db/6_month_in_year_with_highest_sales.sql
@@ -0,0 +1,36 @@
+-- Which month in each year produced the highest cost of sales?
+
+-- The company stakeholders want assurances that the company has been doing well recently.
+
+-- Find which months in which years have had the most sales historically.
+
+-- The query should return the following information:
+
++-------------+------+-------+
+| total_sales | year | month |
++-------------+------+-------+
+|    27936.77 | 1994 |     3 |
+|    27356.14 | 2019 |     1 |
+|    27091.67 | 2009 |     8 |
+|    26679.98 | 1997 |    11 |
+|    26310.97 | 2018 |    12 |
+|    26277.72 | 2019 |     8 |
+|    26236.67 | 2017 |     9 |
+|    25798.12 | 2010 |     5 |
+|    25648.29 | 1996 |     8 |
+|    25614.54 | 2000 |     1 |
++-------------+------+-------+
+
+SELECT ROUND(
+        SUM(product_price * product_quantity)::NUMERIC
+    ) AS total_sales,
+    year,
+    month
+FROM orders_table
+    JOIN dim_store_details ON dim_store_details.store_code = orders_table.store_code
+    JOIN dim_products ON dim_products.product_code = orders_table.product_code
+    JOIN dim_date_times ON dim_date_times.date_uuid = orders_table.date_uuid
+GROUP BY year,
+    month
+ORDER BY total_sales DESC
+LIMIT 10;
