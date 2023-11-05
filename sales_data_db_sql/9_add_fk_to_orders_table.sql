69cc41ae95500b92f1ba0e59774a3fcc63e2346c
Patrick Chan <q_p18@hotmail.com>
2023-11-05 06:21:06 +0000

    Add foreign keys to orders_table.

---
+++ sales_data_db_sql/9_add_fk_to_orders_table.sql
@@ -0,0 +1,21 @@
+-- In the orders_table table of the sales_data database:
+
+-- Add foreign key date_uuid.
+ALTER TABLE orders_table
+ADD FOREIGN KEY (date_uuid) REFERENCES dim_date_times (date_uuid);
+
+-- Add foreign key user_uuid.
+ALTER TABLE orders_table
+ADD FOREIGN KEY (user_uuid) REFERENCES dim_users (user_uuid);
+
+-- Add foreign key card_number.
+ALTER TABLE orders_table
+ADD FOREIGN KEY (card_number) REFERENCES dim_card_details (card_number);
+
+-- Add foreign key store_code.
+ALTER TABLE orders_table
+ADD FOREIGN KEY (store_code) REFERENCES dim_store_details (store_code);
+
+-- Add foreign key product_code.
+ALTER TABLE orders_table
+ADD FOREIGN KEY (product_code) REFERENCES dim_products (product_code);
